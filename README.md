ff09f8d11cf4ce02e05cc91a881fa6a32b637e90
thamudi <tamim.hamoudi@gmail.com>
2021-04-20 09:05:31 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,53 @@
+# Code Challenge
+
+## Authors
+
+Tamim Hamoudi (thamudi), Rawan Nujoom, Dario Thornhill
+
+___
+
+## Exam set up
+
+- Fork this repository
+- Clone your forked repository into your computer
+- cd into the folder that was created
+- run the command :
+  - ```npm install```
+  - This will install all the required packages that you will need to work with.
+- To test if your code is working use ``` npm run test ```
+- You should solve your challenges inside [challenge.test.js](challenge.test.js)
+- DO NOT change any of the test units
+
+___
+
+## Exam Instructions
+
+You will have 30 minutes to solve these code challenges:
+
+- You will find both challenges descriptions inside your [challenge.test.js](challenge.test.js)
+
+After you are placed in your room you will need to do the following: ❗
+
+Share your screen ⬅️ ❗  
+Open your camera ⬅️ ❗  
+Start recording yourself. ⬅️ ❗  
+After You are done with the code challenges you will need to submit the following to your instructor: ❗  
+
+Your github forked repo ‼️  
+Your recording ‼️  
+
+No viewing of previous code is allowed; not event your own code challenges solutions. ❌  
+
+No communication is allowed (Slack, Facebook, whatsapp, smoke signals ..etc) ❌  
+
+No notes are allowed:  
+
+github gist ❌  
+notes ❌  
+.txt notes ❌  
+google sheets ...etc ❌  
+including paper notes ❌  
+You are allowed to use google for searching ✔️  
+
+Do not go to sites which have repositorties such as (github.com, bitbucket.com, gitlab.com) ❌  
+The only sites allowed from github is this repo that you will be forking into your own account.  
---
+++ challenge.test.js
@@ -0,0 +1,58 @@
+'use strict';
+
+/* ------------------------------------------------------------------------------------------------
+CHALLENGE 1
+Write a function that accepts an array of strings. Return the longest string.
+------------------------------------------------------------------------------------------------ */
+
+const longestString = (arr) => {
+    // Solution code here...
+};
+
+/* ------------------------------------------------------------------------------------------------
+CHALLENGE 2
+
+Write a function called isSimilar which takes two strings and returns true if they contain the same characters and false otherwise; if for example the two strings have the same characters and they are rearranged it will return true.
+examples:
+
+isSimilar("form","from")
+=> true
+isSimilar("farm","from")
+=> false
+isSimilar("cat","act")
+=> true
+------------------------------------------------------------------------------------------------ */
+
+const isSimilar = (arr) => {
+    // Solution code here...
+};
+
+/* ------------------------------------------------------------------------------------------------
+TESTS
+
+All the code below will verify that your functions are working to solve the challenges.
+
+DO NOT CHANGE any of the below code.
+
+Run your tests from the console: jest challenges-01.test.js
+
+------------------------------------------------------------------------------------------------ */
+
+describe('Testing challenge 1', () => {
+    test('It should return the string that is the longest in length', () => {
+        expect(longestString(['hello', 'future', '401', 'students', '!'])).toStrictEqual('students');
+        expect(longestString(['rubber', 'ducks', 'dont', 'quack', '!'])).toStrictEqual('rubber');
+    });
+});
+
+describe('Testing challenge 2', () => {
+    test('It Should return true if the string contains the same characters and false if otherwise', () => {
+        expect(isSimilar(['from', 'from'])).toStrictEqual(true);
+        expect(isSimilar(['farm', 'from'])).toStrictEqual(false);
+        expect(isSimilar(['cat', 'act'])).toStrictEqual(true);
+        expect(isSimilar(['cat', 'at'])).toStrictEqual(false);
+        expect(isSimilar(['duck', 'ckud'])).toStrictEqual(true);
+        expect(isSimilar(['rubber', 'duck'])).toStrictEqual(false);
+        expect(isSimilar([])).toStrictEqual(false);
+    });
+});
